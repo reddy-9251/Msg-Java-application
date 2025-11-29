00323da3f18091f52e4d55ffa221cc0293451e47
Kirsn <Kiran03@gmail.com>
2025-11-29 17:12:55 +0000

    created by kiran

--- Msg.java
+++ Msg.java
@@ -1,6 +1,8 @@
 public class Msg{
 	public static void main(string[] args){
 		system.out.println("this is message application done by krishna");
+		system.out.println("New feature add by kiran");
+		system.out.println("Welcome to java application");
 	}
 }
 
